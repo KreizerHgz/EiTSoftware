2203eb3ca9478a6739fcf744ed5495e0a881cdd3
KreizerHgz <37186178+KreizerHgz@users.noreply.github.com>
2021-03-17 18:18:49 +0100

    Bootstrap js added

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -85,12 +85,9 @@ const createWindow = async () => {
     if (!mainWindow) {
       throw new Error('"mainWindow" is not defined');
     }
-    if (process.env.START_MINIMIZED) {
-      mainWindow.minimize();
-    } else {
-      mainWindow.show();
-      mainWindow.focus();
-    }
+    mainWindow.show();
+    mainWindow.maximize();
+    mainWindow.focus();
   });
 
   mainWindow.on('closed', () => {
