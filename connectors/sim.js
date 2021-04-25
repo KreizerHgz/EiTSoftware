b35a44f2cbcc7379646f7792d092a5ec33e0c584
KreizerHgz <37186178+KreizerHgz@users.noreply.github.com>
2021-04-25 13:20:59 +0200

    Ready for numerical script

--- connectors/sim.js
+++ connectors/sim.js
@@ -28,7 +28,7 @@ function connect() {
 
     document.getElementById("gif").style.display = "inline"
 
-    var testRetrieval = new PythonShell('testRetrieval.py', options);
+    var testRetrieval = new PythonShell('simulationScript.py', options);
     testRetrieval.on('message', function (message) {
         CreateCharts();
     });
@@ -36,29 +36,29 @@ function connect() {
 
 function CreateCharts() {
     var img1 = document.getElementById("testImage1");
-    img1.style.width = "200px"
-    img1.style.height = "200px"
-    img1.src = "./images/yoda.jpeg"
+    img1.style.width = "400px"
+    img1.style.height = "320px"
+    img1.src = "./images/figure.png"
     var cap1 = document.getElementById("img1Caption");
     cap1.innerHTML = "Test1"
 
     var img2 = document.getElementById("testImage2");
-    img2.style.width = "200px"
-    img2.style.height = "200px"
+    img2.style.width = "400px"
+    img2.style.height = "320px"
     img2.src = "./images/OL.png"
     var cap2 = document.getElementById("img2Caption");
     cap2.innerHTML = "Test2"
 
     var img3 = document.getElementById("testImage3");
-    img3.style.width = "200px"
-    img3.style.height = "200px"
+    img3.style.width = "400px"
+    img3.style.height = "320px"
     img3.src = "./images/bike.png"
     var cap3 = document.getElementById("img3Caption");
     cap3.innerHTML = "Test3"
 
     var img4 = document.getElementById("testImage4");
-    img4.style.width = "200px"
-    img4.style.height = "200px"
+    img4.style.width = "400px"
+    img4.style.height = "320px"
     img4.src = "./images/sq.png"
     var cap4 = document.getElementById("img4Caption");
     cap4.innerHTML = "Test4"
